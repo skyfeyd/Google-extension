4829cd1746429c2f37d1f138767c8e3bddc3dc2a
Raphaël <raphael.castelnau@edu.devinci.fr>
2021-09-17 18:02:20 +0200

    :construction: work on badge

--- override-devinci.js
+++ override-devinci.js
@@ -1,1 +1,6 @@
-document.getElementsByTagName(body)[0].classList.add("override-body")
+document.getElementsByTagName('body')[0].classList.add("override-body")
+
+document.getElementsByTagName('badge')[0].classList.add("overide-badge")
+
+
+
